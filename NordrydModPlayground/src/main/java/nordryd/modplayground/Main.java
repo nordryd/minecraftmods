bca037d8498304a3a82682d5b2b4137e57fe4962
nordryd <nordryd13@gmail.com>
2019-03-31 18:43:13 -0500

    Added first item - start_item

--- NordrydModPlayground/src/main/java/nordryd/modplayground/Main.java
+++ NordrydModPlayground/src/main/java/nordryd/modplayground/Main.java
@@ -3,11 +3,17 @@ package nordryd.modplayground;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import nordryd.modplayground.lists.ModItems;
 import nordryd.modplayground.util.Reference;
 
 @Mod(Reference.MOD_ID)
@@ -15,21 +21,36 @@ public class Main
 {
 	public static Main instance;
 	private static final Logger logger = LogManager.getLogger(Reference.MOD_ID);
-	
+
 	public Main() {
 		instance = this;
-		
+
 		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
 		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientRegistries);
-		
+
 		MinecraftForge.EVENT_BUS.register(this);
 	}
-	
-	private void setup(final FMLCommonSetupEvent event){
+
+	private void setup(final FMLCommonSetupEvent event) {
 		logger.info("setup() method registered");
 	}
-	
+
 	private void clientRegistries(final FMLClientSetupEvent event) {
 		logger.info("clientRegistries() method registered");
+	}
+
+	@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+	public static class RegistryEvents
+	{
+		@SubscribeEvent
+		public static void registerItems(final RegistryEvent.Register<Item> event) {
+			event.getRegistry()
+					.registerAll(ModItems.start_item = new Item(new Item.Properties().group(ItemGroup.MISC)).setRegistryName(location("start_item")));
+			logger.info("Items registerd");
+		}
+
+		private static ResourceLocation location(String name) {
+			return new ResourceLocation(Reference.MOD_ID, name);
+		}
 	}
 }
